3468da6754e593c7eb295267ad9f7825cd1f153e
Shai Efrati <shaief@gmail.com>
2015-08-09 17:53:12 +0300

    Add git blame line (SPC o g)

--- init.el
+++ init.el
@@ -185,6 +185,30 @@ layers configuration."
    )
    (add-hook 'python-mode-hook 'annotate-pdb)
 
+   (defun git-blame-line ()
+     "Runs `git blame` on the current line and
+   adds the commit id to the kill ring"
+     (interactive)
+     (let* ((line-number (save-excursion
+                           (goto-char (point-at-bol))
+                           (+ 1 (count-lines 1 (point)))))
+            (line-arg (format "%d,%d" line-number line-number))
+            (commit-buf (generate-new-buffer "*git-blame-line-commit*")))
+       (call-process "git" nil commit-buf nil
+                     "blame" (buffer-file-name) "-w" "-L" line-arg)
+       (let* ((commit-id (with-current-buffer commit-buf
+                           (buffer-substring 1 9)))
+              (log-buf (generate-new-buffer "*git-blame-line-log*")))
+         (kill-new commit-id)
+         (call-process "git" nil log-buf nil
+                       "log" "-1" "--pretty=%h   %an   %s" commit-id)
+         (with-current-buffer log-buf
+           (message "Line %d: %s" line-number (buffer-string)))
+         (kill-buffer log-buf))
+       (kill-buffer commit-buf)))
+
+   (evil-leader/set-key "o g" 'git-blame-line)
+
    (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
    (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
    (global-set-key (kbd "C-=") 'text-scale-increase)
